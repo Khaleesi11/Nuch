a858a2ac779af3832b0c2eaa1505dd88b61d3d28
Nuch <nuch99@gmail.com>
2025-10-03 19:17:04 +0200

    Mein erstes Commit

---
+++ 2025_10_03/Operatoren/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Operatoren
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
